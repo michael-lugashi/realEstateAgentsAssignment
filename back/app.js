d789934bd8c71f72c6aaa369f97d5d7101d2467f
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-11-12 19:03:50 +0200

    set up server

--- back/app.js
+++ back/app.js
@@ -1,1 +1,23 @@
-'use strict'
+'use strict';
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const port = 3000;
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.dbUri)
+  .then((res) => {
+    app.listen(port, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(`listening on port ${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
